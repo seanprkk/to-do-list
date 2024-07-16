c6c9363c4be8635c46cafa5824ccca9bb07b7e92
seanprkk <seanprkk@gmail.com>
2024-07-16 14:07:39 -0700

    updated note class implementation

--- src/main/model/Note.java
+++ src/main/model/Note.java
@@ -1,19 +1,21 @@
 package model;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 import persistence.Writable;
 
 public class Note implements Writable {
 
     private String text;
-    private LocalDateTime scheduledTime;
+    private LocalDate scheduledDate;
     private boolean flagged;
+    private boolean late;
 
     public Note(String textInput) {
         setText(textInput);
         this.scheduledTime = null;
         this.flagged = false;
+        this.late = false;
     }
 
     public void setText(String textInput) {
@@ -24,16 +26,36 @@ public class Note implements Writable {
         return this.text;
     }
 
-    public void setTime(LocalDateTime timeInput) {
-        this.scheduledTime = timeInput;
+    public void setDate(LocalDate dateInput) {
+        this.scheduledDate = timeInput;
     }
 
-    public LocalDateTime getTime() {
-        return this.scheduledTime;
+    public LocalDate getDate() {
+        return this.scheduledDate;
     }
 
     public void changeFlag() {
         this.flagged = !this.flagged;
+    }
+
+    public void markLate() {
+        this.late = true;
+    }
+
+    public void unmarkLate() {
+        this.late = false;
+    }
+
+    @Override
+    public JSONObject toJson() {
+        JSONObject json = new JSONObject();
+        json.put("text", text);
+        json.put("flagged", flagged);
+        json.put("late", late);
+        json.put("dateYear", getDate().getYear());
+        json.put("dateMonth", getDate().getMonth());
+        json.put("dateDay", getDate().getDayOfMonth());
+        return json;
     }
 
 }
